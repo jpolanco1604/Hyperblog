3ec1434a97d9e081d08d2328aef942911c80f91c
Jose <jpolanco@komax.cl>
2022-09-06 13:49:07 -0300

    primera publicación de html

--- Platzi/testy.py
+++ Platzi/testy.py
@@ -1,1 +1,10 @@
 print('hello')
+price = int(input('ingrese el precio: $'))
+if price > 1:
+    print("love it")
+else:
+    print("too cheap")
+
+    for i in range(1,2):
+        print('El numero es: '+i)
+print ('fin del programa')
